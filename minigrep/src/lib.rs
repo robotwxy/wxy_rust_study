37a3db86d5b01b6cfcf3076c603dac55f1dd7242
wxy <wxy1994812@gmail.com>
2024-01-09 23:57:22 +0800

    Add fn search and test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -5,6 +5,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    for line in search(&config.query, &contents) {
+        println!("line: {line}");
+    }
+
     Ok(())
 }
 
@@ -21,5 +26,31 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Config { query, file_path })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
